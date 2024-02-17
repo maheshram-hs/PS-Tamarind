c15c1e22d5b36511e9e5fdf6be2a5fe874c3062a
Maheshram <smaheshram@yahoo.com>
2024-02-17 11:24:09 +0530

    Add countries to map

--- static/web/js/map.js
+++ static/web/js/map.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var map = L.map('map').setView([0, 0], 2); // Set the initial view
+    var map = L.map('map').setView([20, 0], 2); // Set the initial view
 
     // Add a base map layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Define your predefined countries and their coordinates
     var countries = {
         'India': [20.5937, 78.9629],
-        'Country2': [latitude2, longitude2],
+        'Singapore': [1.3521, 103.8198],
         // Add more countries as needed
     };
 
@@ -18,4 +18,19 @@ document.addEventListener('DOMContentLoaded', function () {
         var coordinates = countries[country];
         L.circleMarker(coordinates, { color: 'purple', radius: 8 }).addTo(map);
     }
+
+    // Load GeoJSON data for country borders
+    fetch('/static/web/data/map.geojson')
+        .then(response => response.json())
+        .then(data => {
+            // Add GeoJSON layer and style countries
+            L.geoJSON(data, {
+                style: {
+                    fillColor: 'purple', // Fill color for countries
+                    fillOpacity: 0.5,    // Opacity of fill color
+                    color: 'black',      // Border color
+                    weight: 1            // Border weight
+                }
+            }).addTo(map);
+        });
 });
--- web/static/web/js/map.js
+++ web/static/web/js/map.js
@@ -9,13 +9,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // Define your predefined countries and their coordinates
     var countries = {
         'India': [20.5937, 78.9629],
-        'Country2': [latitude2, longitude2],
-        // Add more countries as needed
+        'Singapore': [1.3521, 103.8198],
+        'Brunei': [4.5353, 114.7277],
+        'France': [46.603354, 1.888334],
+        'UnitedKingdom': [55.3781, -3.4360],
+        'UnitedStates': [37.0902, -95.7129],
     };
+    
 
     // Loop through the countries and add markers
     for (var country in countries) {
         var coordinates = countries[country];
-        L.circleMarker(coordinates, { color: 'purple', radius: 8 }).addTo(map);
+        L.circleMarker(coordinates, { color: 'purple', radius: 10 }).addTo(map);
     }
 });
