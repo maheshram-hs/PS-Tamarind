c4b47d8ffaedcc846ffbfbabd4424c2f84584265
Maheshram <smaheshram@yahoo.com>
2024-02-23 20:25:04 +0000

    Website live

--- pstamarind/settings.py
+++ pstamarind/settings.py
@@ -24,7 +24,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pstamarind.com', 'www.pstamarind.com']
 
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'underdevelopment',
     'web',
 ]
 
