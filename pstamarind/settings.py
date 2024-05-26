6a861cd0d289ae7fc5d72470cb33eb77cddf51e9
Maheshram <smaheshram@yahoo.com>
2024-05-26 15:51:03 +0530

    Testing

--- pstamarind/settings.py
+++ pstamarind/settings.py
@@ -123,6 +123,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
